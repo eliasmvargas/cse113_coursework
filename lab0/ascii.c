14e052f7eafa8276f65ce1650cff9a31d65e8589
eliasmvargas <147442935+eliasmvargas@users.noreply.github.com>
2023-12-11 21:06:34 -0700

    Add files via upload

---
+++ lab0/ascii.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Elias Vargas
+//Lab 0
+//8/23/23
+// this is supposed to be a dog.. supposed to..
+
+int main(void)
+{
+    printf(" ^.  .^      /\n");
+    printf(" /__/_______/\n");
+    printf("   /       /\n");
+    printf("  /       /\n");
+    return 0;
+}
---
+++ lab0/name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//Elias Vargas
+//Lab 0
+//8/23/23
+
+
+int main(void)
+{
+    printf("EEEEEEEEEEEE   LLLLL        IIIII    !!!\n");
+    printf("EEEEEEEEEEEE   LLLLL        IIIII    !!!\n");
+    printf("EEEE           LLLLL        IIIII    !!!\n");
+    printf("EEEE           LLLLL        IIIII    !!!\n");
+    printf("EEEEEEEEEE     LLLLL        IIIII    !!!\n");
+    printf("EEEEEEEEEE     LLLLL        IIIII    !!!\n");
+    printf("EEEE           LLLLL        IIIII       \n");
+    printf("EEEEEEEEEEEEE  LLLLLLLLLL   IIIII    !!!\n");
+    printf("EEEEEEEEEEEEE  LLLLLLLLLL   IIIII    !!!\n");
+return 0;
+}
